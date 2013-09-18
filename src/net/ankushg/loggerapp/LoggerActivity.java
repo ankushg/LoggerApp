372eeeaa29ca10388f871c3d95bff370afd355ec
Ankush Gupta <amgupt01@gmail.com>
2013-09-18 15:32:47 -0400

    added logging

--- src/net/ankushg/loggerapp/LoggerActivity.java
+++ src/net/ankushg/loggerapp/LoggerActivity.java
@@ -2,14 +2,22 @@ package net.ankushg.loggerapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class LoggerActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class LoggerActivity extends Activity {
+	private static final String TAG = "LoggerActivity";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Log.d(TAG, "THIS IS MY LOGGING LINE --- IT'S GREAT FOR DEBUGGING");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "I got onPause!!!");
+	}
 }
